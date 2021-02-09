ee002ecf9366d0b44e6574d8307c49767ccc107b
test <test>
2021-02-09 16:53:43 +0100

    ajout de test avec PHP Unit

--- testUnit.php
+++ testUnit.php
@@ -3,5 +3,14 @@ use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
 
+        return $stack;
+    }
+    public function testAddition(){
+        $this->assertSame(15 + 5, 20);
+    }
 }
